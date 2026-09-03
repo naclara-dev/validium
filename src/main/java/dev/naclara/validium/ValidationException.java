accc4476f301f5aa79256c2f433a9e604fc4aacd
Ana Clara <anaclrdev@gmail.com>
2026-09-03 17:46:15 -0300

    style: fine adjustments on exception message

--- src/main/java/dev/naclara/validium/ValidationException.java
+++ src/main/java/dev/naclara/validium/ValidationException.java
@@ -15,7 +15,7 @@ public class ValidationException extends RuntimeException {
 
     private static String buildMessage(List<ValidationError> errors) {
         StringBuilder message = new StringBuilder();
-        message.append(String.format("Validation failed with %d error(s).", errors.size()));
+        message.append(lineSeparator() + String.format("Validation failed with %d error(s):", errors.size()) + lineSeparator());
 
         errors.forEach(error -> {
             message.append(error.field() + ": " + error.message() + lineSeparator());
