fc10ad6cc3c993043baf7a0966cfec842b50bf17
Ana Clara <anaclrdev@gmail.com>
2026-09-03 17:13:59 -0300

    feat: add class ValidationException

---
+++ src/main/java/dev/naclara/validium/ValidationException.java
@@ -0,0 +1,26 @@
+package dev.naclara.validium;
+
+import java.util.List;
+
+import static java.lang.System.lineSeparator;
+
+public class ValidationException extends RuntimeException {
+    private static ValidationError error;
+    List<ValidationError> errors;
+
+    public ValidationException(List<ValidationError> errors) {
+        this.errors = errors;
+        super(buildMessage(errors));
+    }
+
+    private static String buildMessage(List<ValidationError> errors) {
+        StringBuilder message = new StringBuilder();
+        message.append(String.format("Validation failed with %d error(s).", errors.size()));
+
+        errors.forEach(error -> {
+            message.append(error.field() + ": " + error.message() + lineSeparator());
+        });
+
+        return message.toString();
+    }
+}
