6edea782b0d840a433aa93dc37ba94e75b9f1b7a
Ana Clara <anaclrdev@gmail.com>
2026-09-09 18:03:08 -0300

    docs: update annotation examples

--- src/main/java/dev/naclara/validium/examples/Main.java
+++ src/main/java/dev/naclara/validium/examples/Main.java
@@ -9,39 +9,12 @@ public class Main {
         User user = new User();
 
         user.name = "Ana Clara";
-        user.email = "contato@naclara.com";
+        user.email = "";
         user.age = 20;
         user.gender = "Other";
         user.bio = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
         user.password = "Validium123";
 
-        Validator.of(user)
-                .field("name")
-                    .required()
-                    .notBlank()
-
-                .field("email")
-                    .required()
-                    .matches("^[^@\\s]+@[^@\\s]+\\.[^@\\s]+$")
-                    .check(
-                            value -> value instanceof String email && email.endsWith("naclara.dev"),
-                            "E-mail must belong to naclara.dev domain."
-                    )
-
-                .field("age")
-                    .min(21)
-                    .max(99)
-
-                .field("gender")
-                    .in("Female", "Male", "Non-Binary")
-
-                .field("bio")
-                    .maxLength(100)
-
-                .field("password")
-                    .minLength(10)
-
-                .validate();
-
+        Validator.of(user).validate();
     }
 }
--- src/main/java/dev/naclara/validium/examples/User.java
+++ src/main/java/dev/naclara/validium/examples/User.java
@@ -1,11 +1,26 @@
 package dev.naclara.validium.examples;
 
+import dev.naclara.validium.annotation.*;
+
 public class User {
+    @Required
+    @NotBlank
     String name;
+
+    @Required
+    @Matches(pattern = "^[^@\\\\s]+@[^@\\\\s]+\\\\.[^@\\\\s]+$")
     String email;
+
+    @Min(size = 21, message = "You must be of legal age.")
     Integer age;
+
+    @OneOf(values = {"Female", "Male", "Non-Binary"})
     String gender;
+
+    @MaxLength(size = 100)
     String bio;
+
+    @MinLength(size = 10)
     String password;
 
     public User() {
